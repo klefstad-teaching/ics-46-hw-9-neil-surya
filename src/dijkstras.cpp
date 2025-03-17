94fabdfa078306404a62fffb74048bcb6ae04e4f
Neil Surya <ansurya@uci.edu>
2025-03-17 00:06:37 +0000

    Potential fix for extracy_shortest_path.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -28,9 +28,12 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     }
     return distance;
 }
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
-    stack<int> s;
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
+    if (distances[destination] == INF) {
+        return path;
+    }
+    stack<int> s;
     for (int v = destination; v != -1; v = previous[v])
         s.push(v);
     while (!s.empty()) {
