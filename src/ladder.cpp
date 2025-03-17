faa9e1c81deb6e709cca94eac1146cf4d3565c3a
Neil Surya <ansurya@uci.edu>
2025-03-17 00:34:27 +0000

    Fixed formatting in print_word_ladder.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -98,9 +98,9 @@ void print_word_ladder(const vector<string>& ladder) {
         cout << "No word ladder found." << endl;
         return;
     }
+    cout << "Word ladder found: "
     for (size_t i = 0; i < ladder.size(); ++i) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " ";
+        cout << ladder[i] << " ";
     }
     cout << endl;
 }
